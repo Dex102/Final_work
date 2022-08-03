6654888fbae4e5a56878f4fa0b1f288bb4648bdf
EgorEvtushenko <ljvbyj0990@gmail.com>
2022-08-03 10:33:29 +0500

    Wrote the main block of the program

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -21,3 +21,16 @@ void ShowStringArray(string[] array)
         Console.Write(array[i] + " ");
     }
 }
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] MyArray = new string[size];
+
+Console.WriteLine("Input elements of array: ");
+for(int i = 0; i < size; i++)
+{
+    MyArray[i] = Console.ReadLine();
+}
+
+ShowStringArray(MyArray);
